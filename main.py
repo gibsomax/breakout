4dfdd4703014d15814a05adf1b01eeb74c726335
Max Gibson <gibsomax@mail.gvsu.edu>
2026-03-10 14:43:19 -0400

    merge

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 import sys
 from settings import SCREEN_WIDTH, SCREEN_HEIGHT
 from game_objects.ball import BALL
+from Design_template import paddle
+from settings import velocity
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Breakout")
@@ -13,6 +15,8 @@ clock = pygame.time.Clock()
 ball = [BALL()]
 ball_destroy = []
 start = False
+paddle_pos = (SCREEN_WIDTH * .45)
+
 
 running = True
 while running:
@@ -29,6 +33,22 @@ while running:
     if keys[pygame.K_SPACE]:
         start = True
 
+    # exit
+    if keys[pygame.K_q]:
+        pygame.quit()
+
+    # paddle movement
+    if keys[pygame.K_LEFT] and paddle_pos > 0:
+        paddle_pos = paddle_pos - velocity
+        paddle(paddle_pos, SCREEN_HEIGHT * .9, SCREEN_WIDTH, SCREEN_HEIGHT)
+        pygame.display.update()
+    if keys[pygame.K_RIGHT] and paddle_pos < SCREEN_WIDTH - (SCREEN_WIDTH * 0.16):
+        paddle_pos = paddle_pos + velocity
+        paddle(paddle_pos, SCREEN_HEIGHT * .9, SCREEN_WIDTH, SCREEN_HEIGHT)
+        pygame.display.update()
+    else:
+        paddle(paddle_pos, SCREEN_HEIGHT * .9, SCREEN_WIDTH, SCREEN_HEIGHT)
+        pygame.display.update()
 
 
     # clear screen
